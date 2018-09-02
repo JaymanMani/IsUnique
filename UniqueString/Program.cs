ea17f3b3ee803ddd1b0b4b1a61abf189bb0edcc4
v-jayakm <v-jayakm@HIUSA66726>
2018-09-02 10:25:22 -0700

    check-in IsUnique project

---
+++ UniqueString/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace UniqueString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(IsUniqueChars("abcdef"));
+            Console.Read();
+        }        
+
+        private static bool IsUniqueChars(string str)
+        {
+            //remove extra white space
+            str = str.Trim();
+
+            //return false if the input argument is invalid.
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+
+            //return flase if the input string length greater than 128 characters.
+            if (str.Length > 128)
+            {
+                return false;
+            }            
+
+            bool[] char_set = new bool[128];
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char_set[str[i]])
+                {
+                    return false;
+                }
+
+                char_set[str[i]] = true;
+            }
+
+            return true;
+        }
+    }
+}
